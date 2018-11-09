1701295b929fea4dda7f0a026f7a8eaaec6bdb49
Grigory Bartosh <grishabartosh@yandex.ru>
2018-11-10 02:00:06 +0300

    datavase main

---
+++ java/src/main/java/ru/memoscope/dataBase/PostLink.java
@@ -0,0 +1,22 @@
+package ru.memoscope.dataBase;
+
+public class PostLink {
+    private long groupId;
+    private long postId;
+
+    public void setGroupId(long groupId) {
+        this.groupId = groupId;
+    }
+
+    public long getGroupId() {
+        return groupId;
+    }
+
+    public void setPostId(long postId) {
+        this.postId = postId;
+    }
+
+    public long getPostId() {
+        return postId;
+    }
+}
