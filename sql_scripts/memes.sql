6b6b8fe0dd3656ae96cccdff94494afca96bda1b
iisuslik <al.s.fedotov1998@yandex.ru>
2018-11-10 17:41:07 +0300

    fuck

--- sql_scripts/memes.sql
+++ sql_scripts/memes.sql
@@ -1,4 +1,4 @@
-DROP TABLE  IF EXISTS RawPosts, AnalizedPosts CASCADE;
+DROP TABLE  IF EXISTS RawPosts, AnalizedPosts, Timestamps CASCADE;
 
 
 CREATE TABLE RawPosts (
@@ -7,6 +7,7 @@ CREATE TABLE RawPosts (
 	timestamp BIGINT NOT NULL,
 	text TEXT NOT NULL,
 	photoPaths TEXT NOT NULL,
+	sended BOOLEAN NOT NULL,
 	PRIMARY KEY (groupId, postId)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
@@ -23,3 +24,8 @@ CREATE TABLE AnalizedPosts (
 CREATE INDEX timestamp ON AnalizedPosts(timestamp);
 CREATE INDEX groupId ON AnalizedPosts(groupId);
 CREATE FULLTEXT INDEX text ON AnalizedPosts(text);
+
+CREATE TABLE Timestamps (
+	minTimestamp BIGINT NOT NULL,
+	maxTimestamp BIGINT NOT NULL
+);
